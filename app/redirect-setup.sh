a26d05fce242ec87ff002f510960c2734e71895f
Jun Kai Zhang <junkaizhang8@gmail.com>
2024-08-22 01:37:11 -0400

    Implement redirect script

---
+++ app/redirect-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+[ ! -f ./dist/browser/index.html ] && echo "index.html not found" && exit 1
+tail -n 1 ./dist/browser/index.html | grep -q "<!-- Redirect setup complete -->" && echo "Redirect script already set up. Terminating." && exit 1
+[ ! -f redirect-script ] && echo "redirect-script not found" && exit 1
+head -n -1 ./dist/browser/index.html > temp.txt
+mv temp.txt ./dist/browser/index.html
+cat redirect-script >> ./dist/browser/index.html
